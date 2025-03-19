48ca515653fd2d57cadcda7b2104c7f9462652df
SARTHAK RANA <23510370.dit@dypvp.edu.in>
2025-03-19 20:56:49 +0530

    Time: 33 ms (50.49%) | Memory: 73.6 MB (35.90%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> mpp;
+        for (int num : nums) {
+            if (++mpp[num] > 1) return true; 
+        }
+        return false;
+    }
+};
