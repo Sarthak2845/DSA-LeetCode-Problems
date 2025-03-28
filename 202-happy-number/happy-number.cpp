66fb5c2cf5dfd3b03abfcb1304ed9ad4a2865a96
SARTHAK RANA <23510370.dit@dypvp.edu.in>
2025-03-28 09:11:56 +0530

    Time: 2 ms (26.29%) | Memory: 8.4 MB (37.63%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> usedIntegers;
+        while(true){
+            int sum=0;
+            while(n!=0){
+                sum+=pow(n%10,2);
+                n=n/10;
+            }
+            if(sum==1) return true;
+            if (usedIntegers.find(sum) != usedIntegers.end()) return false;
+            usedIntegers.insert(sum);
+            n = sum;
+        }
+    }
+};
