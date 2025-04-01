6bcc84812c01a2ce271468e36c14cfd0503cce68
SARTHAK RANA <23510370.dit@dypvp.edu.in>
2025-04-01 19:31:55 +0530

    Time: 0 ms (100.00%) | Memory: 119.7 MB (86.16%) - LeetSync

---
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+   long long mostPoints(vector<vector<int>>& questions) {
+    int n = questions.size();
+    vector<long long> dp(n + 1, 0);
+
+    for (int i = n - 1; i >= 0; i--) {
+        int skipIdx = i + questions[i][1] + 1;  
+        long long take = questions[i][0] + (skipIdx < n ? dp[skipIdx] : 0);   
+        long long skip = dp[i + 1];  
+        dp[i] = max(take, skip);  
+    }
+
+    return dp[0];  
+}
+};
