72716978b2b09d36a463493e348200f453310787
SARTHAK RANA <23510370.dit@dypvp.edu.in>
2025-06-21 09:32:44 +0530

    Time: 3 ms (98.27%) | Memory: 11.3 MB (83.10%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        // Step 1: Detect if cycle exists
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                // Step 2: Find the start of the cycle
+                slow = head;
+                while (slow != fast) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+        }
+
+        return nullptr; // No cycle
+    }
+};
