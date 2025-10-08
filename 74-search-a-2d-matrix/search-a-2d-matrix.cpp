b33edf53a91105703cdd7c800e16a84d3bb74b66
SARTHAK RANA <23510370.dit@dypvp.edu.in>
2025-10-08 23:14:37 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (42.67%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int target) {
+        int r = 0;
+        int c = mat[0].size() - 1;
+        while (r < mat.size() && c >= 0) {
+            if (mat[r][c] == target)
+                return true;
+            else if (mat[r][c] > target)
+                c--;
+            else
+                r++;
+        }
+        return false;
+    }
+};
