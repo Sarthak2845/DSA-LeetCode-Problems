fbb70285199f7576ae4a124354571a1555372db7
SARTHAK RANA <23510370.dit@dypvp.edu.in>
2025-11-12 22:23:25 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (21.25%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+          vector<int> ans;
+    ans.reserve(2 * n); // optional optimization
+
+    for (int i = 0; i < n; i++) {
+        ans.push_back(nums[i]);     // xi
+        ans.push_back(nums[i + n]); // yi
+    }
+
+    return ans;
+    }
+};
