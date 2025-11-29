8edce25ee642b3e073444bff4910e683c72ce4ad
SARTHAK RANA <23510370.dit@dypvp.edu.in>
2025-11-29 22:51:04 +0530

    Time: 0 ms (100.00%) | Memory: 35.8 MB (54.21%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
-    int pivotIndex(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> sumLeft(n, 0);
-        vector<int> sumRight(n, 0);
-
-        // Build sumLeft
-        for (int i = 1; i < n; ++i) {
-            sumLeft[i] = sumLeft[i - 1] + nums[i - 1];
+    int pivotIndex(vector<int>& arr) {
+        int n=arr.size();
+        long long total=accumulate(arr.begin(),arr.end(),0LL);
+        long long leftsum=0;
+        for(int i=0;i<n;i++){
+            long long rightsum=total-leftsum-arr[i];
+            if(leftsum==rightsum) return i;
+            leftsum+=arr[i];
         }
-
-        for (int i = n - 2; i >= 0; --i) {
-            sumRight[i] = sumRight[i + 1] + nums[i + 1];
-        }
-
-
-        for (int i = 0; i < n; ++i) {
-            if (sumLeft[i] == sumRight[i]) {
-                return i;
-            }
-        }
-
         return -1;
     }
 };
