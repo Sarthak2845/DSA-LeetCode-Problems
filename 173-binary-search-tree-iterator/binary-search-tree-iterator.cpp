3af441886d55d95e64f82f471cd4de578a340516
SARTHAK RANA <23510370.dit@dypvp.edu.in>
2026-03-01 12:02:47 +0530

    Time: 6 ms (32.21%) | Memory: 31.9 MB (92.98%) - LeetSync

---
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.cpp
@@ -0,0 +1,27 @@
+class BSTIterator {
+private:
+    stack<TreeNode*> st;
+
+    void pushAll(TreeNode* root) {
+        while (root != nullptr) {
+            st.push(root);
+            root = root->left;
+        }
+    }
+
+public:
+    BSTIterator(TreeNode* root) {
+        pushAll(root);
+    }
+    
+    int next() {
+        TreeNode* temp = st.top();
+        st.pop();
+        pushAll(temp->right);
+        return temp->val;   
+    }
+    
+    bool hasNext() {
+        return !st.empty();
+    }
+};
