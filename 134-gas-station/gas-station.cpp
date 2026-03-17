d58161797634b076cc9a75bc538e3d51e1e7a539
SARTHAK RANA <23510370.dit@dypvp.edu.in>
2026-03-17 20:50:05 +0530

    Time: 0 ms (100.00%) | Memory: 112.5 MB (60.25%) - LeetSync

---
+++ 134-gas-station/gas-station.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int totalGas = accumulate(gas.begin(), gas.end(), 0);
+        int totalCost = accumulate(cost.begin(), cost.end(), 0);
+
+        if (totalGas < totalCost) return -1;
+
+        int total = 0;
+        int res = 0;
+
+        for (int i = 0; i < gas.size(); i++) {
+            total += gas[i] - cost[i];
+
+            if (total < 0) {
+                res = i + 1;
+                total = 0;
+            }
+        }
+
+        return res;
+    }
+};
