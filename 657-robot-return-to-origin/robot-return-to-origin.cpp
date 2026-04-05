983c651863a9a4353a5cf734381e7c0b1f7f7118
SARTHAK RANA <23510370.dit@dypvp.edu.in>
2026-04-05 19:51:16 +0530

    Time: 5 ms (15.29%) | Memory: 10.2 MB (95.43%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x=0;
+        int y=0;
+        for(char c:moves){
+            if(c=='U'){
+                y+=1;
+            }
+            else if(c=='D'){
+                y-=1;
+            }
+            else if(c=='L'){
+                x-=1;
+            }
+            else{
+                x+=1;
+            }
+        }
+        return ((x==0) && (y==0));
+    }
+};
