11292845ad2b0e84f3fc60ec61f1d4f3d106f091
SARTHAK RANA <23510370.dit@dypvp.edu.in>
2026-04-07 23:13:01 +0530

    Time: 29 ms (67.24%) | Memory: 128.4 MB (31.90%) - LeetSync

---
+++ 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.cpp
@@ -0,0 +1,33 @@
+class Robot {
+public:
+    int W, H, P;
+    int pos = 0;
+    bool hasMoved = false;
+
+    Robot(int width, int height) {
+        W = width - 1;
+        H = height - 1;
+        P = 2 * (W + H);
+    }
+    
+    void step(int num) {
+        hasMoved = true;
+        pos = (pos + num) % P;
+    }
+    
+    vector<int> getPos() {
+        if(pos <= W) return {pos, 0};
+        if(pos <= W + H) return {W, pos - W};
+        if(pos <= 2 * W + H) return {W - (pos - (W + H)), H};
+        return {0, H - (pos - (2 * W + H))};
+    }
+    
+    string getDir() {
+        if(! hasMoved) return "East";
+        if(pos == 0) return "South";
+        if(pos <= W) return "East";
+        if(pos <= W + H) return "North";
+        if(pos <= 2 * W + H) return "West";
+        return "South";
+    }
+};
